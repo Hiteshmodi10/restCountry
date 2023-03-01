43177f49c9fde972522e3368f4b5c26cb4bb4696
Hitesh <hiteshmodi81287@gmail.com>
2023-03-01 22:58:02 +0530

    new update

--- script.js
+++ script.js
@@ -99,11 +99,10 @@ let boxArea = document.querySelectorAll(".country-area");
 let modal = document.querySelector(".modal");
 let countryNameDetail = document.querySelectorAll(".country-allname");
 let alltext = document.querySelectorAll(".text");
+let loading = document.querySelector(".nation-text")
 
 function addPage() {
-  setTimeout(() => {
     newPage.style.display = "block";
-  }, 10);
 }
 
 function removePage() {
@@ -131,15 +130,11 @@ newSearchcity.addEventListener("keypress", function (e) {
 });
 
 function cityInfo() {
-  if (searchCity.value != "") {
     countryDetail();
-    // addPage();
-  }
+    loading.textContent = "loading ................"
 }
 function newcityInfo() {
-  if (newSearchcity.value != "") {
     countryDetailNew();
-  }
 }
 
 
@@ -157,9 +152,10 @@ function countryDetail() {
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
+     
       if (
         response.status !== 404 &&
-        searchCity.value != searchCity.value.substring(0, 4)
+        searchCity.value != searchCity.value.substring(0, 3)
       ) {
         addPage();
         if (searchCity.value == "china" || searchCity.value == "China") {
@@ -171,7 +167,7 @@ function countryDetail() {
           capital.textContent = response[0].capital;
           currency.textContent = response[0].currencies[0].name;
           language.textContent = response[0].languages[0].name;
-          area.textContent = response[0].area;
+          area.textContent = `${response[0].area} km square`
           borderCountry.textContent = response[0].borders;
           console.log(response[0].nativeName);
         } else {
@@ -194,7 +190,7 @@ function countryDetail() {
             response[response.length - 1].currencies[0].name;
           language.textContent =
             response[response.length - 1].languages[0].name;
-          area.textContent = response[response.length - 1].area;
+          area.textContent = `${response[response.length - 1].area} km square`;
           borderCountry.textContent = response[response.length - 1].borders;
           // countryName.textContent = response[response.length - 1].name;
           countryName.textContent = searchCity.value;
@@ -214,6 +210,7 @@ function countryDetail() {
         }
       } else {
         alert("pls enter correct country name ");
+        loading.textContent = 'Discover the world of countries'
       }
     });
 }
@@ -234,7 +231,7 @@ function countryDetailNew() {
       console.log(response);
       if (
         response.status !== 404 &&
-        newSearchcity.value != newSearchcity.value.substring(0, 4)
+        newSearchcity.value != newSearchcity.value.substring(0, 3)
       ) {
         if (newSearchcity.value == "china" || newSearchcity.value == "China") {
           countryName.textContent = response[0].name;
@@ -318,3 +315,66 @@ if (window.screen.width <= 920) {
   }
 
 }
+
+
+//auto complete  suggestion
+
+
+
+function autocomplete(inp, arr) {
+
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the country names in the world:*/
+var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+
+autocomplete(searchCity, countries);
