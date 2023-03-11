d523fa7573a5f8b258b8f498f77d78cbef77dda7
Hitesh <hiteshmodi81287@gmail.com>
2023-03-11 08:28:33 +0530

    new change

--- script.js
+++ script.js
@@ -1,78 +1,7 @@
 let newPage = document.querySelector(".newPage");
-let newData = [];
 let recentSearch = JSON.parse(localStorage.getItem("recentSearch"));
 let countrySearch = JSON.parse(localStorage.getItem("countrySearch"));
 
-newPage.innerHTML = `
-  <nav>
-      <div class="logo">
-        <p class="logo-text">Where in the world?</p>
-      </div>
-    </nav>
-    <div class="loading-container" style="
-    height: 40px;
-">
-    </div>
-    <main class= "main-container">
-      <div class="back-home">
-        <button class="back-btn">
-          <img src="./img/back-arrow.png" alt="" class="back-arrow" />
-          <span class="back">back</span>
-        </button>
-      </div>
-      <div class="search-container">
-        <input type="text" placeholder="Search country..." id="searchCountry" class="newPageSearch"/>
-
-        <button class="search-btn find-btn">Find</button>
-      </div>
-      <div class="country-details-container">
-        <div class="flag-info">
-          <img class="flag-img" src="https://flagcdn.com/in.svg" alt="" />
-        </div>
-        <div class="country-info-container">
-          <h1 class="country-name">India</h1>
-          <div class="country-detail">
-            <div class="country-left-detail">
-              <p class="native-name">
-                Native Name : <span class="tetx native-name-text"> भारत</span>
-              </p>
-              <p class="population">
-                population : <span class="population-text"> 1380M</span>
-              </p>
-              <p class="Region">
-                Region: <span class="region-text">Asia</span>
-              </p>
-              <p class="sub-region">
-                Sub Region : <span class="sub-region-text">Southern Asia</span>
-              </p>
-              <p class="capital">
-                capital : <span class="capital-text">New Delhi</span>
-              </p>
-            </div>
-            <div class="country-right-detail">
-              <p class="currency">
-                currancy : <span class="currency-text"> Indian rupee</span>
-              </p>
-              <p class="language">
-                language : <span class="language-text">Hindi</span>
-              </p>
-              <p class="area">area : <span class="area-text">3M</span></p>
-            </div>
-          </div>
-          
-        </div>
-       
-      </div>
-      <div class="border-country">
-      <p class="border">
-        borders Country :
-        <span class="border-country-text"
-          >AFG,BGD,BTN,MMR,CHN,NPL,PAK,LKA</span>
-      </p>
-    </div>
-    </main>     
-
-  `;
 homepage();
 
 let searchCity = document.querySelector("#searchCountry");
@@ -295,10 +224,8 @@ function addPage() {
 function removePage() {
   newPage.style.display = "none";
   searchCity.value = "";
+  loading.style.display = "none";
 }
-
-searchBtn.addEventListener("click", countryDetail);
-newSearchbtn.addEventListener("click", countryDetailNew);
 
 backButton.addEventListener("click", function () {
   removePage();
@@ -306,256 +233,172 @@ backButton.addEventListener("click", function () {
 
 searchCity.addEventListener("keypress", function (e) {
   if (e.key == "Enter") {
-    countryDetail();
+    countryDetail(searchCity);
   }
 });
 newSearchcity.addEventListener("keypress", function (e) {
   if (e.key == "Enter") {
-    countryDetailNew();
+    countryDetail(newSearchcity);
   }
 });
 
-let data = [];
-
-function countryDetail() {
+function countryDetail(cityName) {
   someCountryCondition();
-  data.unshift(searchCity.value);
-  if (searchCity.value == searchCity.value.substring(0, 2)) {
+  if (cityName.value == cityName.value.substring(0, 2)) {
     alert("please enter correct country name");
-  } else if (searchCity.value == "") {
+  } else if (cityName.value == "") {
     alert("pls enter country name");
   } else {
-    scanApi();
+   
+    if (cityName !== searchCity) {
+      scanApi(newSearchcity);
+    }
+    else{
+      scanApi(searchCity);
+    }
     loading.innerHTML = `<img src="https://media.tenor.com/UnFx-k_lSckAAAAM/amalie-steiness.gif" alt="" style="
     height: 59px ">`;
   }
-  setLocalHistory();
-  
+  // setLocalHistory(searchCity);
+  // setLocalHistory(newSearchcity);
 }
 
-function countryDetailNew() {
-  someCountryCondition();
-  data.unshift(newSearchcity.value);
-  if (newSearchcity.value == newSearchcity.value.substring(0, 2)) {
-    alert("please enter correct country name");
-  } else if (newSearchcity.value == "") {
-    alert("pls enter country name");
-  } else {
-    document.querySelector(".main-container").style.display = "none";
-    document.querySelector(".loading-container").style.display = "block";
-    scanApi();
-    document.querySelector(
-      ".loading-container"
-    ).innerHTML = `<img src="https://media.tenor.com/UnFx-k_lSckAAAAM/amalie-steiness.gif" alt=""  class = "loading-img">`;
-    newSearchcity.value == ""
-  }
-  setLocalHistoryNew();
-}
+searchBtn.addEventListener("click", function () {
+  countryDetail(searchCity);
+});
+newSearchbtn.addEventListener("click", function () {
+  countryDetail(newSearchcity);
+});
 
 function someCountryCondition() {
-  if (searchCity.value == "america" || searchCity.value == "America" || newSearchcity.value == "America" || newSearchcity.value == "america") {
+  if (
+    searchCity.value == "america" ||
+    searchCity.value == "America" ||
+    newSearchcity.value == "America" ||
+    newSearchcity.value == "america"
+  ) {
     searchCity.value = "United States of America";
     newSearchcity.value = "United States of America";
   }
-  if (searchCity.value == "england" || searchCity.value == "England" || newSearchcity.value == "England" || newSearchcity.value == "england") {
+  if (
+    searchCity.value == "england" ||
+    searchCity.value == "England" ||
+    newSearchcity.value == "England" ||
+    newSearchcity.value == "england"
+  ) {
     searchCity.value = "United kingdom";
     newSearchcity.value = "United kingdom";
   }
 }
 
-function scanApi() {
-  fetch(`https://restcountries.com/v2/name/${data[0]} `)
+function scanApi(cityName) {
+  fetch(`https://restcountries.com/v2/name/${cityName.value} `)
     .then((response) => response.json())
     .then((response) => {
-        addPage();
-        loading.textContent = "Discover the world of countries";
-        document.querySelector(".main-container").style.display = "block";
-        document.querySelector(".loading-container").style.display = "none";
-        if (searchCity.value == "china" || searchCity.value == "China" || newSearchcity.value == "China" || newSearchcity.value == "china") {
-          response.length = 1;
+      console.log(response);
+      if (response.status !== 404) {
+        const values = response.values();
+        for (const value of values) {
+          if (
+            value.name.toLowerCase().substring(0, 3) ==
+            cityName.value.toLowerCase().substring(0, 3)
+          ) {
+            countryName.textContent = value.name;
+
+            flagimg.src = value.flag;
+
+            if (Number(value.population) > 1000000)
+             { 
+              population.textContent = `${Math.trunc(Number(value.population) / 1000000)}M`;
+            }
+             else {
+              population.textContent = value.population;
+            }
+            nativeName.textContent = value.nativeName;
+
+            region.textContent = value.region;
+
+            subRegion.textContent = value.subregion;
+
+            capital.textContent = value.capital;
+
+            currency.textContent = value.currencies[0].name;
+
+            language.textContent = value.languages[0].name;
+
+            area.textContent = `${value.area} km square`;
+
+            borderCountry.textContent = value.borders;
+
+            if (Number(value.population) > 1000000) {
+              population.textContent = `${Math.trunc(
+                Number(value.population) / 1000000
+              )}M`;
+            } else {
+              population.textContent = value.population;
+            }
+
+            if (borderCountry.textContent == "") {
+              borderCountry.textContent = "Country border the ocean";
+            }
+
+            function setLocalStorage() {
+              for (let i = 0; i < recentSearch.length; i++) {
+                boxCountryName[i].textContent = value.name;
+                boxNativename[i].textContent = value.nativeName;
+
+                boxFlagImg[i].src = value.flags.png;
+                // console.log(value.flags.png);
+
+                if (Number(value.population) > 1000000) {
+                  boxPopulation[i].textContent = `${Math.trunc(
+                    Number(value.population) / 1000000
+                  )}M`;
+                } else {
+                  boxPopulation[i].textContent = value.population;
+                }
+
+                boxRegion[i].textContent = value.region;
+
+                boxCapitalName[i].textContent = value.capital;
+
+                boxCurrency[i].textContent = value.currencies[0].name;
+
+                boxLanguage[i].textContent = value.languages[0].name;
+
+                boxArea[i].textContent = `${value.area} km sq`;
+              }
+            }
+            setLocalStorage();
+            addPage();
+          }
         }
-        {
-          countryName.textContent = response[response.length - 1].name;
-          flagimg.src = response[response.length - 1].flag;
-          if (Number(response[response.length - 1].population) > 1000000) {
-            population.textContent = `${Math.trunc(
-              Number(response[response.length - 1].population) / 1000000
-            )}M`;
-          } else {
-            population.textContent = response[response.length - 1].population;
-          }
-          nativeName.textContent = response[response.length - 1].nativeName;
-          region.textContent = response[response.length - 1].region;
-          subRegion.textContent = response[response.length - 1].subregion;
-          capital.textContent = response[response.length - 1].capital;
-          currency.textContent =
-            response[response.length - 1].currencies[0].name;
-          language.textContent =
-            response[response.length - 1].languages[0].name;
-          area.textContent = `${response[response.length - 1].area} km square`;
-          borderCountry.textContent = response[response.length - 1].borders;
-
-          flagimg.src = response[response.length - 1].flag;
-
-          if (Number(response[response.length - 1].population) > 1000000) {
-            population.textContent = `${Math.trunc(
-              Number(response[response.length - 1].population) / 1000000
-            )}M`;
-          } else {
-            population.textContent = response[response.length - 1].population;
-          }
-
-          if (borderCountry.textContent == "") {
-            borderCountry.textContent = "Country border the ocean";
-          }
-        }
-      } 
-    ).catch((e)=> {
-      alert("pls enter correct country name ");
-      loading.textContent = "Discover the world of countries";
-      document.querySelector(".loading-container").style.display = "none";
-      document.querySelector(".main-container").style.display = "block";
+      } else if (response.status == 404) {
+        alert("pls enter correct country name");
+        loading.style.display = "none";
+        searchCity.value = ""
+      }
+    }).catch(() => {
+      console.log('server error: ' + 404);
     })
+
+  // loading.textContent = "Discover the world of countries";
+  // document.querySelector(".main-container").style.display = "block";
+  // document.querySelector(".loading-container").style.display = "none";
 }
-
-
- 
-function setNewpageLocalstorage() {
-  scanApi();
-}
-
 for (let i = 0; i < countryBox.length; i++) {
   countryBox[i].addEventListener("click", function () {
     searchCity.value = boxCountryName[i].textContent;
     addPage();
-    countryDetail()
-    setLocalHistory()
+    countryDetail(searchCity);
   });
 }
 
-//auto complete  suggestion
-
-function autocomplete(inp, arr) {
-  var currentFocus;
-  inp.addEventListener("input", function (e) {
-    var a,
-      b,
-      i,
-      val = this.value;
-    closeAllLists();
-    if (!val) {
-      return false;
-    }
-    currentFocus = -1;
-    a = document.createElement("DIV");
-    a.setAttribute("id", this.id + "autocomplete-list");
-    a.setAttribute("class", "autocomplete-items");
-    this.parentNode.appendChild(a);
-    for (i = 0; i < arr.length; i++) {
-      /*check if the item starts with the same letters as the text field value:*/
-      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-        /*create a DIV element for each matching element:*/
-        b = document.createElement("DIV");
-        /*make the matching letters bold:*/
-        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-        b.innerHTML += arr[i].substr(val.length);
-        /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-        /*execute a function when someone clicks on the item value (DIV element):*/
-        b.addEventListener("click", function (e) {
-          /*insert the value for the autocomplete text field:*/
-          inp.value = this.getElementsByTagName("input")[0].value;
-          /*close the list of autocompleted values,
-          (or any other open lists of autocompleted values:*/
-          closeAllLists();
-        });
-        a.appendChild(b);
-      }
-    }
-  });
-  function closeAllLists(elmnt) {
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
+function setLocalHistory(cityName) {
+  if (cityName.value != cityName.value.substring(0, 2)) {
+    newData.unshift(cityName.value);
   }
-  /*execute a function when someone clicks in the document:*/
-  document.addEventListener("click", function (e) {
-    closeAllLists(e.target);
-  });
-}
-
-/*An array containing all the country names in the world:*/
-
-var countries = [ "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre & Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts & Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe", ];
-
-autocomplete(searchCity, countries);
-autocomplete(newSearchcity, countries)
-// if(window.matchMedia("(max-width: 700px)")) {
-  
-// }
-
-function setLocalHistory() {
-  if (searchCity.value != searchCity.value.substring(0, 2)) {
-    newData.unshift(searchCity.value);
-  }
-  if (newData != "") {
+  if (newData !== "") {
     localStorage.setItem("recentSearch", JSON.stringify(newData));
   }
-
-}
-function setLocalHistoryNew() {
-  if (newSearchcity.value != newSearchcity.value.substring(0, 2)) {
-    
-    newData.unshift(newSearchcity.value);
-  }
-  if (newData != "") {
-    localStorage.setItem("recentSearch", JSON.stringify(newData));
-  }
-}
-if (newData != '') {
-      for (let i = 0; i < recentSearch.length; i++) {
-        fetch(`https://restcountries.com/v2/name/${recentSearch[i]}`)
-        .then((response) => response.json())
-        .then((response) => {
-        if (recentSearch[i] == "China" || recentSearch[i] == "china") {
-          response.length = 1;
-          console.log(response.length);
-        }
-        if (response.status != "404") {
-          boxCountryName[i].textContent = response[response.length - 1].name;
-          boxNativename[i].textContent =
-            response[response.length - 1].nativeName;
-
-          boxFlagImg[i].src = response[response.length - 1].flags.png;
-          // console.log(response[response.length - 1].flags.png);
-
-          if (Number(response[response.length - 1].population) > 1000000) {
-            boxPopulation[i].textContent = `${Math.trunc(
-              Number(response[response.length - 1].population) / 1000000
-            )}M`;
-          } else {
-            boxPopulation[i].textContent =
-              response[response.length - 1].population;
-          }
-
-          boxRegion[i].textContent = response[response.length - 1].region;
-
-          boxCapitalName[i].textContent =
-            response[response.length - 1].capital;
-
-          boxCurrency[i].textContent =
-            response[response.length - 1].currencies[0].name;
-
-          boxLanguage[i].textContent =
-            response[response.length - 1].languages[0].name;
-
-          boxArea[i].textContent = `${
-            response[response.length - 1].area
-          } km sq`;
-        }
-      })
-    };
 }
